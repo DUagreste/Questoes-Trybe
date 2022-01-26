e4c6a05bdd6ef960f0c2bcf204f2ab5cda732e36
Vítor Pinheiro <85112141+DUagreste@users.noreply.github.com>
2022-01-25 23:33:52 -0300

    Add files via upload

---
+++ Quest_2.py
@@ -0,0 +1,11 @@
+# Questão 02
+
+chances = list()
+contador = int(input("Insira quantos elementos deseja acrescentar: "))
+for i in range(1, contador+1):
+    chances.append(int(input("Digite o número: ")))
+    contador += 1
+print(chances)
+
+# Nessa questão eu não fui muito bem, teria que multiplicar os elementos
+# todos por 3, e eu ainda não sei como faria isso.
---
+++ Quest_3.py
@@ -0,0 +1,9 @@
+# Questão 03
+
+frase = input("Escreva uma frase: ")
+letra = input("Selecione uma letra: ")
+
+print(f"A letra selecionada aparece {frase.count(letra)} vez(es)")
+
+# Essa foi a mais fácil, já que o Python já tem uma função adequada.
+# No processo seletivo deixei até mais simples.
